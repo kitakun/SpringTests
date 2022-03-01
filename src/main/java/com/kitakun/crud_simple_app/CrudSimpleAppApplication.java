8bab9084abedea2c257cec4953122da3f4fda613
kitakun <xfikysx@ya.ru>
2022-03-01 17:17:12 +0300

    * initial app with index page

---
+++ src/main/java/com/kitakun/crud_simple_app/CrudSimpleAppApplication.java
@@ -0,0 +1,12 @@
+package com.kitakun.crud_simple_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSimpleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSimpleAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kitakun/crud_simple_app/CrudSimpleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitakun.crud_simple_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSimpleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
